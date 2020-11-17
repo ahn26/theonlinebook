6ec76e5c6a77df3d123cbbd902d48e334199ccb0
TranKai <quyettran057@gmail.com>
2020-11-17 12:39:24 +0700

    new updated code!

--- src/com/team/controller/ListBestSellerController.java
+++ src/com/team/controller/ListBestSellerController.java
@@ -4,24 +4,25 @@ import java.io.IOException;
 import java.util.List;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.team.dao.impl.CategoryDaoImpl;
-import com.team.model.Category;
+import com.team.dao.impl.OrderedDaoImpl;
+import com.team.model.Ordered;
 
 /**
- * Servlet implementation class ShoppingCartController
+ * Servlet implementation class ListBestSellerController
  */
-
-public class ShoppingCartController extends HttpServlet {
+@WebServlet("/ListBestSellerController")
+public class ListBestSellerController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	/**
 	 * @see HttpServlet#HttpServlet()
 	 */
-	public ShoppingCartController() {
+	public ListBestSellerController() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
@@ -32,10 +33,13 @@ public class ShoppingCartController extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		List<Category> listCategory = new CategoryDaoImpl().getAll();
 
-		request.setAttribute("listcategory", listCategory);
-		request.getRequestDispatcher("/view/user/template/shopping-cart.jsp").forward(request, response);
+		List<Ordered> listBestSeller = new OrderedDaoImpl().getBestSeller();
+
+		request.setAttribute("listproduct", listBestSeller);
+
+		request.getRequestDispatcher("/view/user/template/shop.jsp").forward(request, response);
+
 	}
 
 }
